c7bc375c2e0eb6b28d2c5dfa362bf553b751d428
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-09-07 22:54:10 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+'usestrict';
+let element7 = document.getElementById('e7'); // span
+let word1 = element7.innerHTML; // you
+let element8 = document.getElementById('e8'); // span
+let word2 = element8.innerHTML; // can
+let element16 = document.getElementById('e16'); // li
+let word3 = element16.innerHTML; // turn
+let element9 = document.getElementById('e9'); // p
+let word4 = element9.innerHTML; // back
+let element18 = document.getElementById('e18'); // li
+let word5 = element18.innerHTML; // pages
+let element13 = document.getElementById('e13'); // strong
+let word6 = element13.innerHTML; // but
+let element12 = document.getElementById('e12'); // em
+let word7 = element12.innerHTML; // not
+let element2 = document.getElementById('e2'); // time
+let word8 = element2.innerHTML; // time
+
+console.log(`${word1} ${word2} ${word3} ${word4} ${word5} ${word6} ${word7} ${word8}`);
+// you can turn back pages but not time
